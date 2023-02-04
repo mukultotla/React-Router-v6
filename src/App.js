c6243f4a2a22963130057328d43e01c1138c6fdb
Mukul <mukultotla4@gmail.com>
2023-02-05 01:21:27 +0530

    router v6

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import {Link, Route, Routes} from "react-router-dom"
+import { Book } from "./pages/Book";
+import { BookList } from "./pages/BookList";
+import { Home } from "./pages/Home";
+import { NewBook } from "./pages/NewBook";
+import { NotFound } from "./pages/NotFound";
 function App() {
   return (
-    <h1>
-      Hello World!
-    </h1>
+    <>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/books">Books</Link>
+        </li>
+      </ul>
+    </nav>
+   <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/books">
+        <Route index element={<BookList/>} />
+        <Route path=":id" element={<Book/>}/>
+        <Route path="new" element={<NewBook/>}/>
+      </Route>
+      <Route path="*" element={<NotFound/>}></Route>
+   </Routes>
+   </>
   );
 }
 
